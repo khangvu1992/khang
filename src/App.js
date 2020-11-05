3a77ed5ee0effcb38aa47bedd60de4c0da71cd81
vubichlan1 <vubichlan1@gmail.com>
2020-11-05 12:26:41 +0700

    them file

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+
+function App() {
+  const [arr, setarr] = useState("");
+
+  const drop=(e)=>{
+    const a= e.target.value
+    console.log(a)
+   setarr(a)
+   
+  
+   
+   
+  }
+  const [arr2, setarr2] = useState([""]);
+
+  const clich = (e)=>{
+    e.preventDefault();
+
+    setarr2(arr2.concat(arr))
+    setarr("")
+     console.log(arr2)}
+  return (
+    <div >
+       <form >
+        <input type="text" onChange={drop} value={arr} />
+        <label for="">thoi ve lai tu dau</label>
+        <input type="file" /> 
+        <button type="submit" onClick={clich} >sumit</button>
+        <ul >
+        {arr2.map(x=>( <li>{x}</li>)) }
+        </ul>
+    
+      </form>
+    </div>
+  );
+}
+
+export default App;
