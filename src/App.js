d0d0212942e21bcdd66f95c31c20f85525a77b4e
Your Name <you@example.com>
2020-11-07 09:46:06 +0700

    hahh

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { useState ,useEffect} from 'react';
 import axios from 'axios'
 
 function App() {
-  const [notes, setNotes] = useState([])
-  
-  useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3000/ten')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
+  const [notes, setNotes] = useState("")
+  const [notes1, setNotes1] = useState([])
+
  
+    // axios.get('http://localhost:3000/ten').then(res => setNotes1(res.data.he) )
+
+
+  
+  const onChange1 = (e)=>{
+    setNotes(e.target.value)
+  }
+  
+  const onClick1 = ()=>{
+
+    let moi = { he: notes  }
+    axios.post('http://localhost:3000/ten',moi).then(res => {
+    setNotes1(notes1.concat( res.data))
+    
+  })
+    setNotes("")
+    
+  }
   return (
     <div >
+      <input type="text" onChange={onChange1}  value={notes}/>
      ghgh
-      {notes.he}
+     <button onClick={onClick1}>nhap</button>
+      {notes1.map(a=>
+        <li>{a.he}</li>
+        )}
     </div>
   );
 }
